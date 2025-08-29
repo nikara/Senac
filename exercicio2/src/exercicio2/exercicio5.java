74b3d3e548807d846d43c5fb33157f902da9284e
NikolasLamin <62860064+nikara@users.noreply.github.com>
2025-08-29 19:15:36 -0300

    Correções
    Correções

--- exercicio2/src/exercicio2/exercicio5.java
+++ exercicio2/src/exercicio2/exercicio5.java
@@ -5,20 +5,20 @@ import java.util.Scanner;
 // 5) Crie um programa que receba uma String e imprima linha a linha suas letras.
 
 public class exercicio5 {
+    public static void main(String[] args) {
+        
+    	Scanner sc = new Scanner(System.in);
+    	try {
+        String texto = sc.nextLine();
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		System.out.println("\n\tDigite uma palavra");
-		
-		String palavra;
-		palavra = sc.nextLine();
-		
-		for(int i = 0; i < palavra.length(); i++) {
-			System.out.println(palavra.charAt(i));
-		}
-		
-		
-
-	}
-
+        for(int i = 0; i < texto.length(); i++){
+            System.out.println(texto.charAt(i));
+        }
+    	}catch(Exception e) {
+    		
+    	}finally {
+    		sc.close();
+    	}
+        
+    }
 }
