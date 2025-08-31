1fc5bdb2ba2b85e2d3e9d30f64e393995ba0d013
NikolasLamin <62860064+nikara@users.noreply.github.com>
2025-08-31 19:54:00 -0300

    Update exercicio4.java

--- exercicio3/src/exercicio3/exercicio4.java
+++ exercicio3/src/exercicio3/exercicio4.java
@@ -2,12 +2,35 @@ package exercicio3;
 
 import java.util.Scanner;
 
+import java.math.BigInteger;
+// 4) Crie um programa que execute uma operação de Exponenciação com o valor inteiro com precisão arbitrária.
 public class exercicio4 {
+	
+	static void ExpRap(BigInteger base,BigInteger exp) {
+		BigInteger res = BigInteger.ONE;
+		
+		while(exp.compareTo(BigInteger.ZERO) > 0) {
+			
+			if(exp.testBit(0)) {
+				res = res.multiply(base);
+			}
+			base = base.multiply(base);
+			exp = exp.shiftRight(1);
+		}
+		System.out.println("Resultado: " + res);
+	}
+	
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
+		BigInteger base = new BigInteger("3");
+		BigInteger expoente = new BigInteger("13");
+		
+		
 		try {
+
+			ExpRap(base,expoente);
 			
 		}catch(Exception e) {
 			System.out.println("Erro: " + e.getMessage());
